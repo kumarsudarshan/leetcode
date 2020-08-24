9bf335278c48d9a665e122b96df9cbc924f94374
kumarsudarshan <sudarshansingh9@gmail.com>
2020-08-24 23:01:48 +0530

    Added sum of left leaves

---
+++ src/leetcode/augustchallenge/SumOfLeftLeaves.java
@@ -0,0 +1,41 @@
+package leetcode.augustchallenge;
+
+import leetcode.prerequisite.TreeNode;
+
+/*
+ * Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+ * 
+ */
+
+public class SumOfLeftLeaves {
+	int result = 0;
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(3);
+		root.left = new TreeNode(9);
+		root.right = new TreeNode(20);
+		root.right.left = new TreeNode(15);
+		root.right.right = new TreeNode(7);
+		System.out.println(new SumOfLeftLeaves().sumOfLeftLeaves(root));
+	}
+	// recursive
+	public int sumOfLeftLeaves(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		if (root.left != null && root.left.left == null && root.left.right == null) {
+			return root.left.val + sumOfLeftLeaves(root.right);
+		}
+		return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+	}
+}
